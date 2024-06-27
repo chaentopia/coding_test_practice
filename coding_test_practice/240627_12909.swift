a749710b39f1ec699381daed9a8e96b0550b3b6e
정채은 <cce523@hanmail.net>
2024-06-27 17:18:24 +0900

    solved: 프로그래머스 #12909

--- coding_test_practice/240627_12909.swift
+++ coding_test_practice/240627_12909.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+func solution(_ s:String) -> Bool
+{
+    var ans:Bool = false
+    var openCount : Int = 0
+    var closeCount : Int = 0
+    var string = s.map { String($0) }
+    var isStartOpen : Bool = true
+    
+    string.forEach {
+        if $0 == "(" {
+            openCount += 1
+        } else if $0 == ")" {
+            if openCount != 0 {
+                openCount -= 1
+            } else {
+                isStartOpen = false
+            }
+        }
+    }
+    
+    if isStartOpen {
+        ans = openCount == 0 ? true : false
+    } else {
+        ans = false
+    }
+    return ans
+}
